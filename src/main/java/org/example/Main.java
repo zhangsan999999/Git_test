0d212bf92590f9ec70c02c52d8824219ff41bccc
“fangfang” <1736525203@qq.com>
2023-08-29 21:08:24 +0800

    hello文件测试12

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hahahah");
+        System.out.println("log");
         System.out.println("Hello world!");
     }
 }
